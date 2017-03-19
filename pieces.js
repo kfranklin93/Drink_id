b28de886771da062d9143f9058298c4fd591cd0c
Nerajno <Nerajno@gmail.com>
2017-03-19 16:55:22 -0400

    working in progress

--- pieces.js
+++ pieces.js
@@ -2,16 +2,21 @@ function myFunction() {
   var today = new Date();
   var year = today.getFullYear();
   var remainder = 21;
-  var urStuff = document.getElementById("birthYr").value;
-
+  var urStuff = $("#birthYr").val();
   //urStuff=Number(birthYr);
    /* onclick = take the input and minus it from today's date and if more than 21 give 1 message (yes,you can) else another message (you shall not pass) */
   //console.log(year - urStuff);
 
-  if (year - birthYr > remainder){
-   console.log("yes, drink");
+ //Esterlin- helped
+  if (year - urStuff > remainder){
+   document.getElementById('two');
  }else{
-   console.log("nope");
+   document.getElementById('three');
  };
-//getElementById within or instead of the console log. 
+
+ console.log(remainder);
+//getElementById within or instead of the console log.
 };
+
+$('.letSee').on("click",myFunction());
+//Making progress 
