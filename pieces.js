5e735544c10741f5290be1bb452db316b7389dfa
Nerajno <Nerajno@gmail.com>
2017-03-19 18:53:01 -0400

    Still @ it

--- pieces.js
+++ pieces.js
@@ -1,22 +1,28 @@
+// this is untidy as hell.
+
 function myFunction() {
   var today = new Date();
   var year = today.getFullYear();
   var remainder = 21;
   var urStuff = $("#birthYr").val();
+  var ableToDrnk = year - urStuff;
+  var other = year - ableToDrnk;
   //urStuff=Number(birthYr);
    /* onclick = take the input and minus it from today's date and if more than 21 give 1 message (yes,you can) else another message (you shall not pass) */
   //console.log(year - urStuff);
 
  //Esterlin- helped
-  if (year - urStuff > remainder){
-   document.getElementById('two');
+  if (other > remainder){
+   document.getElementById('.two');
+  //console.log("hi");
  }else{
-   document.getElementById('three');
+   //console.log("bye");
+   document.getElementById('.three');
  };
 
- console.log(remainder);
+ console.log(year);
 //getElementById within or instead of the console log.
 };
-
-$('.letSee').on("click",myFunction());
-//Making progress 
+// was trying to figure this out another way 
+//$('.letSee').on("click",myFunction());
+//Making progress
